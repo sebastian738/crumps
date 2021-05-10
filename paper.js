9a71d1d017c00f99e61c9273242179448654e1cb
sebastian738 <78593792+sebastian738@users.noreply.github.com>
2021-05-10 22:42:25 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,30 @@
+class Paper{
+    constructor(x,y,r){
+var opt = {
+    isStatic:false,
+    restitution : 0.3,
+    friction : 0,
+    density: 1.2
+}
+this.x =x 
+this.y = y 
+this.r = r
+this. image = loadImage("paper.png")
+this.body = Bodies.circle(this.x, this.y, this.r, opt)
+
+    }
+    display(){
+        var pos = this.body.position
+        push ()
+
+     translate(pos.x , pos.y)
+     imageMode(CENTER)
+     image (this.image, 0,0,this.r, this.r)
+
+
+
+
+
+        pop ()
+    }
+}
